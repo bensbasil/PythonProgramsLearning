bf76ed6150417ad1240fbcc48ff92bacbb6474be
bensbasil <bensdbasil@gmail.com>
2025-09-18 11:07:29 +0530

    Number Game added

---
+++ Number Game.py
@@ -0,0 +1,21 @@
+import random
+def number_game():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    secret_number = random.randint(1, 100)
+    attempts=0
+    while True:
+        try:
+            guess=int(input("Enter your number:"))
+            attempts+=1
+            if guess<secret_number:
+                print("TOO LOW ! try again")
+            elif guess>secret_number:
+                print("TOO HIGH ! try again")
+            else:
+                print(f"Congratulations! You've guessed the number {secret_number} in {attempts} attempts.")
+                break
+        except ValueError:
+            print("Please enter a valid integer.")
+if __name__=="__main__":
+    number_game()
